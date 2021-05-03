abd1ca5f651e1752bdcebfa2201544c81b369238
VadimKominch <kominch.vadim@yandex.ru>
2021-05-03 13:27:46 +0300

    add basic swagger configuration

---
+++ src/main/java/com/mastery/java/task/config/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.mastery.java.task.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Simple web application Api")
+                                .version("1.0.0")
+                                .contact(new Contact()
+                                .name("Kominch Vadim")
+                                .email("kominch.vadim@yandex.ru")
+                                )
+                );
+    }
+}
